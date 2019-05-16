72e7eae3b9904a5f7d33aa84677fba1365893cc6
longzhaoqiang <13787928471@163.com>
2019-05-16 18:01:15 +0800

    课程删改

--- juxuecheng/src/main/resources/static/js/myself/course/course.js
+++ juxuecheng/src/main/resources/static/js/myself/course/course.js
@@ -4,24 +4,28 @@ $(function () {
     $.ajax({
         url: url,
         type: "POST",
-        data: {"bussId":bussId},
+        data: {"bussId": bussId},
         success: function (data) {
             var code = data.resultCode;
             var msg = data.resultMsg;
             var result = data.result;
             var course_str = '';
-            if (code == "1"){
-                for (var i = 0;i < result.length;i++) {
-                    course_str += "<div class='course-1'><a id="+result[i]['id']+" onclick='javascript:course_info(this)'>" +
+            if (code == "1") {
+                for (var i = 0; i < result.length; i++) {
+                    course_str += "<div class='course-1'><a id=" + result[i]['id'] + " onclick='javascript:course_info(this)'>" +
                         "                <div style='height: 110px'>" +
-                        "                    <img src='/img/myself/course_bg.jpg' style='width: 90%'>" +
+                        "                    <img src='/img/myself/course_bg.jpg' style='width: 90%;border-radius: 5px;'>" +
                         "                    <div class='course-2'>" +
-                        "                        <p>"+data['result'][i]['courseName']+"</p>" +
+                        "                        <p class='course-color'>" + data['result'][i]['courseName'] + "</p>" +
                         "                    </div>" +
-                        "                </div>" +
+                        "                </div></a>" +
+                        "                <div class='course-name' style='width: 90%'>" +
+                        "                <span>" + data['result'][i]['courseName'] + "</span></div>" +
                         "                <div style='width: 90%'>" +
-                        "                    <span class='course-3'>" + data['result'][i]['courseName'] + "</span>" +
+                        "                    <span class='c-blue glyphicon glyphicon-pencil' id=" + result[i]['id'] + " onclick='update_course(this)'></span>" +
+                        "                    <span class='course-color glyphicon glyphicon-trash' id=" + result[i]['id'] + " onclick='delete_course(this)'></span>" +
                         "                </div>" +
+                        "<hr style='filter: alpha(opacity=100,finishopacity=0,style=3);border-top: 3px solid #eee;' width='100%' color='#6f5499'/>" +
                         "            </div>";
                 }
                 $("#courseList").html(course_str);
@@ -39,5 +43,31 @@ function course_add() {
 // 查询课程详情
 function course_info(obj) {
     var course_id = obj.id;
-    window.location.href = "/course/getCourse?courseId="+course_id;
+    window.location.href = "/course/getCourse?param1=" + course_id;
+}
+
+function update_course(obj) {
+    var course_id = obj.id;
+    window.location.href = "/course/getCourse?param1="+course_id+"&flag=1";
+}
+
+function delete_course(obj) {
+    var id = obj.id;
+    var is_delete = confirm("是否删除?");
+    if (is_delete) {
+        var url = "/course/delete";
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: {"id" : id},
+            success: function (data) {
+                var code = data.resultCode;
+                if (code == "1"){
+                    window.location.reload(this);
+                } else {
+                    alert("网络异常！请稍后再试");
+                }
+            }
+        })
+    }
 }
